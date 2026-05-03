d474cef301fde0eb230a266b10b8afbf4ed1a159
garciamartinezpersonaltrainer-max <garciamartinezpersonaltrainer@gmail.com>
2026-05-03 17:34:00 -0400

    Add files via upload

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import AppWrapper from './AppWrapper.jsx'
+import App from './App.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <AppWrapper />
+    <App />
   </React.StrictMode>
 )
