4e78e056bd4d0fbb66c2455189a3adac4fa854a8
garciamartinezpersonaltrainer-max <garciamartinezpersonaltrainer@gmail.com>
2026-05-05 00:08:46 -0400

    Add files via upload

--- src/Rutinas.jsx
+++ src/Rutinas.jsx
@@ -165,7 +165,7 @@ const VideoPlayer = ({ url, nombre }) => {
             ✕ Cerrar
           </button>
           <iframe
-            src={`https://www.youtube.com/embed/${videoId}?autoplay=1&rel=0`}
+            src={`https://www.youtube-nocookie.com/embed/${videoId}?autoplay=1&rel=0&modestbranding=1`}
             title={nombre}
             width="100%"
             height="200"
